22392490f88868c17de9cac39a4467e80e23b341
Aneesh Dogra <lionaneesh@gmail.com>
2013-12-04 10:28:48 +0530

    Add bilinear interpolation in Circular LBP.

--- Circular-LBP.py
+++ Circular-LBP.py
@@ -3,6 +3,15 @@ import cv2
 from matplotlib import pyplot as plt
 import math
 
+def bilinear_interpolation(x, y, img):
+    x1, y1 = int(r), int(c)
+    x2, y2 = math.ceil(r), math.ceil(c)
+
+    r1 = (x2 - x) / (x2 - x1) * get_pixel_else_0(img, x1, y1) + (x - x1) / (x2 - x1) * get_pixel_else_0(img, x2, y1)
+    r2 = (x2 - x) / (x2 - x1) * get_pixel_else_0(img, x1, y2) + (x - x1) / (x2 - x1) * get_pixel_else_0(img, x2, y2)
+
+    return (y2 - y) / (y2 - y1) * r1 + (y - y1) / (y2 - y1) * r2
+
 def thresholded(center, pixels):
     out = []
     for a in pixels:
@@ -32,7 +41,7 @@ for x in range(0, len(img)):
         for point in range(1, P + 1):
             r = x + R * math.cos(2 * math.pi * point / P)
             c = y - R * math.cos(2 * math.pi * point / P)
-            pixels.append(get_pixel_else_0(img, int(r), int(c))) # XXX: fix this!
+            pixels.append(int(bilinear_interpolation(r, c, img)))
 
         values = thresholded(center, pixels)
         res = 0
